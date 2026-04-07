78f0e77110d92bb7347897ecb0cdbf9d08fa9068
wuyichao71 <wuyichao71@126.com>
2026-04-07 16:04:28 +0000

    Deploying to gh-pages from @ wuyichao71/lab-page@0b9a6b7e827b7c0f621418156ceee0336c7d8122 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://wuyichao71.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://wuyichao71.github.io/lab-page/#contact">contact us</a>.'
               }
             ]
           }
